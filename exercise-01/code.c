5e8c32b5011a03df4d0f5ec5e339077a572a92e4
Simone Stefani <s.stefani95@gmail.com>
2017-10-31 15:35:27 +0100

    Complete ex01 1.2

--- exercise-01/code.c
+++ exercise-01/code.c
@@ -1,12 +1,27 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+char global[] = "This is a global string";
 
 int main() {
 
-    foo:
+    int pid = getpid();
 
+    const int read_only = 123456;
+
+    foo:
+        printf("process id: %d\n", pid);
+        printf("global string: %p\n", &global);
+        printf("read only:  %p\n", &read_only);
         printf("the code: %p\n", &&foo);
 
-        for(;;) {}
+        printf("\n\n /usr/bin/vmmap %d\n\n", pid);
+        char command[50];
+        sprintf(command, "vmmap %d", pid);
+        system(command);
+
         return 0;
         
 }
